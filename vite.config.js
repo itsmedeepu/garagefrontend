7a1b46f99b8932659f6fffec9c2a24f2d037925f
Deepak S <deepak.github@outlook.com>
2025-02-09 09:59:28 +0530

    intial commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/garage",
+  base: "/garage/",
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 100000000,
